b0b0d2de881283fb73541bc391a36b7c97781e4d
ullu <ullu@ullu.de>
2020-07-01 01:25:39 +0200

    Make module for NLP.
    Make a TitleProcessor class that
    will do the prep of the title string.
    Language guess and filtering already
    in place.

--- lang_process.py
+++ lang_process.py
@@ -1,1 +1,36 @@
+import nltk
+from nltk.classify import TextCat
+from nltk.corpus import udhr
 
+
+class TitleProcessor:
+    def __init__(self, title_list, languages, nltk_path):
+        self.title_list = title_list
+        self.processed = []
+        self.languages = languages
+        # guesses for all titles; kind of for debug purposes
+        self.lang_guesses = []
+        nltk.data.path.append(nltk_path)
+
+    def _guessLanguage(self):
+        textcat = TextCat()
+        for title in self.title_list:
+            lang = textcat.guess_language(title)
+            self.lang_guesses.append((title, lang))
+
+    def _filterUndesiredLang(self):
+        for title_lang in self.lang_guesses:
+            if title_lang[1] in self.languages:
+                self.processed.append(title_lang[0])
+
+    def cleanTitles(self, title_list):
+        """Clean titles of punctuation
+
+        Args:
+            title_list (list): list of cleaned title strings
+        """
+        pass
+
+    def processTitles(self):
+        self._guessLanguage()
+        self._filterUndesiredLang()
--- predict.py
+++ predict.py
@@ -1,13 +1,31 @@
 import os
 import sys
 
+import nltk
+from nltk.classify import TextCat
+
 from ytscrape import getYTTitles
+from lang_process import TitleProcessor
 
 WEBDRIVER_PATH = "/home/linuser/data/utils/webdrivers/geckodriver"
 PROF_PATH = "/home/linuser/.mozilla/firefox/xpdj4t5a.default"
+NLTK_PATH = "/home/linuser/data/utils/nltk/"
+LANGS = ["eng", "deu"]
 
 if __name__ == "__main__":
     # if len(sys.argv) < 2:
     #     print("{} [path to webdriver binary] [path to firefox profile]".format(sys.argv[0]))
     titles = getYTTitles(WEBDRIVER_PATH, PROF_PATH)
     print(titles)
+
+    # nltk.data.path.append("/home/linuser/data/utils/nltk/")
+
+    # textcat = TextCat()
+    # for title in titles:
+    #     print(textcat.guess_language(title))
+
+    title_processor = TitleProcessor(titles, LANGS, NLTK_PATH)
+    title_processor.processTitles()
+
+    print(title_processor.processed)
+    print(title_processor.lang_guesses)
