4a4acf8aa4661a06e7f81dea664862689697e334
ullu <ullu@ullu.de>
2020-07-07 01:02:00 +0200

    Provide Processor methods.
    Methods in TitleProcessor
    class now do filtering stop
    words, punctuation, non-alnum
    chars, lemmatizing for eng
    and ger.

--- lang_process.py
+++ lang_process.py
@@ -1,6 +1,13 @@
 import nltk
+import re
 from nltk.classify import TextCat
-from nltk.corpus import udhr
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from HanTa import HanoverTagger
+
+# for mapping of strings of Textcat guess to stopwords param
+LANG_MAP = {"eng": "english", "deu": "german"}
 
 
 class TitleProcessor:
@@ -10,27 +17,86 @@ class TitleProcessor:
         self.languages = languages
         # guesses for all titles; kind of for debug purposes
         self.lang_guesses = []
+        # the languages of the titles in self.processed
+        self.selection_langs = []
         nltk.data.path.append(nltk_path)
 
     def _guessLanguages(self):
         textcat = TextCat()
         for title in self.title_list:
             lang = textcat.guess_language(title)
-            self.lang_guesses.append((title, lang))
+            self.lang_guesses.append(lang)
 
-    def _filterUndesiredLang(self):
-        for title_lang in self.lang_guesses:
-            if title_lang[1] in self.languages:
-                self.processed.append(title_lang[0])
+    def _filterDesiredLang(self):
+        for i in range(len(self.title_list)):
+            if self.lang_guesses[i] in self.languages:
+                self.processed.append(self.title_list[i])
+                self.selection_langs.append(self.lang_guesses[i])
 
-    def _cleanTitles(self, title_list):
-        """Clean titles of punctuation
+    def _filterGoodNonAlnum(self):
+        """In a *single* title sort out words/terms to keep as tokens
+        that have non-alphanumeric characters in them. E.g. R&B.
 
         Args:
             title_list (list): list of cleaned title strings
         """
-        pass
+        prefix = "'\"("
+        infix = "&/-"
+        suffix = "%'\")"
+        # TODO:
+
+    def _nltkTag2WordnetTag(self, nltk_tag):
+        if nltk_tag.startswith("J"):
+            return wordnet.ADJ
+        elif nltk_tag.startswith("V"):
+            return wordnet.VERB
+        elif nltk_tag.startswith("N"):
+            return wordnet.NOUN
+        elif nltk_tag.startswith("R"):
+            return wordnet.ADV
+        else:
+            return None
+
+    def _lemmatizeTokens(self, tokens, lang):
+        lemmatizer = WordNetLemmatizer()
+        hanta = HanoverTagger.HanoverTagger("morphmodel_ger.pgz")
+        # tokenize the sentence and find the POS tag for each token
+        if lang == "english":
+            nltk_tagged = nltk.pos_tag(tokens)
+            # tuple of (token, wordnet_tag)
+            wordnet_tagged = map(
+                lambda x: (x[0], self._nltkTag2WordnetTag(x[1])), nltk_tagged
+            )
+            lemmatized = []
+            for word, tag in wordnet_tagged:
+                if tag is None:
+                    # if there is no available tag, append the token as is
+                    lemmatized.append(word)
+                else:
+                    # else use the tag to lemmatize the token
+                    lemmatized.append(lemmatizer.lemmatize(word, tag))
+
+        if lang == "german":
+            tups_taglemmapos = hanta.tag_sent(tokens)
+            lemmatized = [tup[1] for tup in tups_taglemmapos]
+
+        return lemmatized
+
+    def _tokenizeFilterLemma(self):
+        for i in range(len(self.processed)):
+            toks = word_tokenize(self.processed[i])
+            filtered_toks = []
+            for t in range(len(toks)):
+                if toks[t].isalnum():
+                    lang = LANG_MAP[self.selection_langs[i]]
+                    if toks[t] not in stopwords.words(lang):
+                        filtered_toks.append(toks[t])
+            lemmatized = [
+                tok.lower() for tok in self._lemmatizeTokens(filtered_toks, lang)
+            ]
+            self.processed[i] = lemmatized
 
     def processTitles(self):
         self._guessLanguages()
-        self._filterUndesiredLang()
+        self._filterDesiredLang()
+        self._tokenizeFilterLemma()
