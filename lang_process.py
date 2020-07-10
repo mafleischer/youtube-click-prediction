42c0e259c492819ddb48c2b63cd377cef8937332
ullu <ullu@ullu.de>
2020-07-10 02:03:49 +0200

    Split function _tokenizeFilterLemma.
    Make steps seperate funct.s so they can be excluded, if need be.
    Init lemmatizers in constructor.

--- lang_process.py
+++ lang_process.py
@@ -5,13 +5,14 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
 from HanTa import HanoverTagger
+from gensim.models import Word2Vec
 
 # for mapping of strings of Textcat guess to stopwords param
 LANG_MAP = {"eng": "english", "deu": "german"}
 
 
 class TitleProcessor:
-    def __init__(self, title_list, languages, nltk_path):
+    def __init__(self, title_list, languages, nltk_data):
         self.title_list = title_list
         self.processed = []
         self.languages = languages
@@ -19,7 +20,11 @@ class TitleProcessor:
         self.lang_guesses = []
         # the languages of the titles in self.processed
         self.selection_langs = []
-        nltk.data.path.append(nltk_path)
+        nltk.data.path.append(nltk_data)
+        self.lemmatizers = {
+            "english": WordNetLemmatizer(),
+            "german": HanoverTagger.HanoverTagger("morphmodel_ger.pgz"),
+        }
 
     def _guessLanguages(self):
         textcat = TextCat()
@@ -36,7 +41,37 @@ class TitleProcessor:
                 self.processed.append(self.title_list[i])
                 self.selection_langs.append(self.lang_guesses[i])
 
-    def _filterGoodNonAlnum(self):
+    # def _tokenizeFilterLemma(self):
+    #     """Tokenize, filter non-alnum strings, call
+    #     lemmatizing function, make lower case
+
+    #     TODO: call the other filter function which will
+    #     filter on other criteria: keep words like R&B as a
+    #     token.
+    #     """
+    #     for i in range(len(self.processed)):
+    #         toks = word_tokenize(self.processed[i])
+    #         filtered_toks = []
+    #         for t in range(len(toks)):
+    #             if toks[t].isalnum():
+    #                 lang = LANG_MAP[self.selection_langs[i]]
+    #                 if toks[t].lower() not in stopwords.words(lang):
+    #                     filtered_toks.append(toks[t])
+    #         lemmatized = [
+    #             tok.lower() for tok in self._lemmatizeTokens(filtered_toks, lang)
+    #         ]
+    #         self.processed[i] = lemmatized
+
+    def _tokenizeAlnum(self):
+        for i in range(len(self.processed)):
+            toks = word_tokenize(self.processed[i])
+            filtered_toks = []
+            for t in range(len(toks)):
+                if toks[t].isalnum():
+                    filtered_toks.append(toks[t])
+            self.processed[i] = filtered_toks
+
+    def _tokenizeExceptions(self):
         """In a *single* title sort out words/terms to keep as tokens
         that have non-alphanumeric characters in them. E.g. R&B.
 
@@ -47,6 +82,16 @@ class TitleProcessor:
         infix = "&/-"
         suffix = "%'\")"
         # TODO:
+
+    def _removeStopwords(self):
+        for i in range(len(self.processed)):
+            toks = self.processed[i]
+            filtered_toks = []
+            lang = LANG_MAP[self.selection_langs[i]]
+            for t in range(len(toks)):
+                if toks[t].lower() not in stopwords.words(lang):
+                    filtered_toks.append(toks[t])
+            self.processed[i] = filtered_toks
 
     def _nltkTag2WordnetTag(self, nltk_tag):
         """Return corresponding wordnet POS tag
@@ -69,64 +114,39 @@ class TitleProcessor:
         else:
             return None
 
-    def _lemmatizeTokens(self, tokens, lang):
-        """Lemmatize list of tokens in language lang.
-
-        Return lemmatized as list.
-
-        Args:
-            tokens (list): list of tokens
-            lang (str): language the tokens are in
-
-        Returns:
-            list: list of strings
+    def _lemmatize(self):
+        """Lemmatize all lists of tokens in self.processed.
         """
-        lemmatizer = WordNetLemmatizer()
-        hanta = HanoverTagger.HanoverTagger("morphmodel_ger.pgz")
+
         # tokenize the sentence and find the POS tag for each token
-        if lang == "english":
-            nltk_tagged = nltk.pos_tag(tokens)
-            # tuple of (token, wordnet_tag)
-            wordnet_tagged = map(
-                lambda x: (x[0], self._nltkTag2WordnetTag(x[1])), nltk_tagged
-            )
-            lemmatized = []
-            for word, tag in wordnet_tagged:
-                if tag is None:
-                    # if there is no available tag, append the token as is
-                    lemmatized.append(word)
-                else:
-                    # else use the tag to lemmatize the token
-                    lemmatized.append(lemmatizer.lemmatize(word, tag))
-
-        if lang == "german":
-            tups_taglemmapos = hanta.tag_sent(tokens)
-            lemmatized = [tup[1] for tup in tups_taglemmapos]
-
-        return lemmatized
-
-    def _tokenizeFilterLemma(self):
-        """Tokenize, filter non-alnum strings, call
-        lemmatizing function, make lower case
-
-        TODO: call the other filter function which will
-        filter on other criteria: keep words like R&B as a
-        token.
-        """
         for i in range(len(self.processed)):
-            toks = word_tokenize(self.processed[i])
-            filtered_toks = []
-            for t in range(len(toks)):
-                if toks[t].isalnum():
-                    lang = LANG_MAP[self.selection_langs[i]]
-                    if toks[t].lower() not in stopwords.words(lang):
-                        filtered_toks.append(toks[t])
-            lemmatized = [
-                tok.lower() for tok in self._lemmatizeTokens(filtered_toks, lang)
-            ]
+            lang = LANG_MAP[self.selection_langs[i]]
+            lemmatized = []
+            if lang == "english":
+                wordnet = self.lemmatizers[lang]
+                nltk_tagged = nltk.pos_tag(self.processed[i])
+                # tuple of (token, wordnet_tag)
+                wordnet_tagged = map(
+                    lambda x: (x[0], self._nltkTag2WordnetTag(x[1])), nltk_tagged
+                )
+                for word, tag in wordnet_tagged:
+                    if tag is None:
+                        # if there is no available tag, append the token as is
+                        lemmatized.append(word.lower())
+                    else:
+                        # else use the tag to lemmatize the token
+                        lemmatized.append(wordnet.lemmatize(word, tag).lower())
+
+            if lang == "german":
+                hanta = self.lemmatizers[lang]
+                tups_taglemmapos = hanta.tag_sent(self.processed[i])
+                lemmatized = [tup[1].lower() for tup in tups_taglemmapos]
+
             self.processed[i] = lemmatized
 
     def processTitles(self):
         self._guessLanguages()
         self._filterDesiredLang()
-        self._tokenizeFilterLemma()
+        self._tokenizeAlnum()
+        self._removeStopwords()
+        self._lemmatize()
