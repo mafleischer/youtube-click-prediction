75dffb5c95786b2e7919d58ab9dade39d01a0742
ullu <ullu@ullu.de>
2020-07-11 03:32:21 +0200

    Implement basic DB record ops.
    First implementation of DB init, write record, load records,
    check if title present.
    
    TODO: Save raw titles of only desired languages.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import nltk
 from nltk.classify import TextCat
 from nltk.corpus import stopwords
 
+from store_load import DB
 from ytscrape import getTNVideoInfo
 from lang_process import TitleProcessor
 
@@ -18,10 +19,17 @@ if __name__ == "__main__":
     #     print("{} [path to webdriver binary] [path to firefox profile]".format(sys.argv[0]))
     tninfo = getTNVideoInfo(WEBDRIVER_PATH, PROF_PATH)
     titles = [tup[1] for tup in tninfo]
-    # print(tninfo)
+    print(tninfo)
     print(titles)
 
     title_processor = TitleProcessor(titles, LANGS, NLTK_DATA)
     title_processor.processTitles()
 
     print(title_processor.processed)
+    db = DB()
+    for tup in tninfo:
+        args = list(tup)
+        db.insertYTRawRecord(args)
+
+    rows = db.loadRaw()
+    print(rows)
--- store_load.py
+++ store_load.py
@@ -2,12 +2,13 @@
 
 import sqlite3
 import joblib
+import os
 
 
 class DB:
     def __init__(self, db_file="ytdata.sqlite3"):
-        self.db_con = sqlite3.connect(db_file)
-        self.yt_col_names = {
+        self.yttable_raw = "ytraw"
+        self.yt_raw_cols = {
             "link": "href",
             "title": "title",
             "channel": "channel",
@@ -15,16 +16,59 @@ class DB:
             "uploaded_time": "uploaded_hrs_ago",
             "target": "clicked",
         }
+        if os.path.isfile(db_file):
+            self.db_con = sqlite3.connect(db_file)
+        else:
+            print("db created")
+            self.db_con = sqlite3.connect(db_file)
+            self._dbInit()
 
-    def insertYTRecord(record):
+    def _dbInit(self):
+        cursor = self.db_con.cursor()
+        sql_create_table_ytraw = """ CREATE TABLE IF NOT EXISTS {} (
+                                        id integer PRIMARY KEY,
+                                        {} text NOT NULL,
+                                        {} text NOT NULL,
+                                        {} text NOT NULL,
+                                        {} integer NOT NULL,
+                                        {} integer NOT NULL,
+                                        {} integer NOT NULL);
+                                """.format(
+            self.yttable_raw, *self.yt_raw_cols.values()
+        )
+        cursor.execute(sql_create_table_ytraw)
+
+    def insertYTRawRecord(self, record):
+        # better avoid hardcoding place of title in record?
+        title_ix = list(self.yt_raw_cols.keys()).index("title")
+        title = record[title_ix]
+        if self.isTitleInDB(title):
+            return
+
+        sql_insert_raw = """INSERT INTO {}({}, {}, {}, {}, {}, {})
+                            VALUES('{}','{}','{}',{},{},{})""".format(
+            self.yttable_raw, *self.yt_raw_cols.values(), *record, 0
+        )
+        cursor = self.db_con.cursor()
+        cursor.execute(sql_insert_raw)
+
+    def loadRaw(self, cols="*"):
+        sql_select_all_raw = """SELECT {} FROM {};""".format(cols, self.yttable_raw)
+        cursor = self.db_con.cursor()
+        cursor.execute(sql_select_all_raw)
+        return cursor.fetchall()
+
+    def updateRecordTarget(self, title):
         pass
 
-    def loadAll():
-        pass
-
-    def updateRecordTarget(title):
-        pass
-
-    def isTitleInDB(title):
-        pass
+    def isTitleInDB(self, title):
+        sql_select_where_raw = """SELECT {0} FROM {1} WHERE {0} = '{2}';""".format(
+            self.yt_raw_cols["title"], self.yttable_raw, title
+        )
+        cursor = self.db_con.cursor()
+        cursor.execute(sql_select_where_raw)
+        row = cursor.fetchall()
+        if row:
+            return True
+        return False
 
