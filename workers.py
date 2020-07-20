8488b791218ca4556caf45bec16fffcabd8202d1
ullu <ullu@ullu.de>
2020-07-21 01:13:19 +0200

    Create Worker class.
    Holds some attributes.Put worker functions in there.
    
    TODO: maybe structure clearer.

--- workers.py
+++ workers.py
@@ -1,51 +1,58 @@
 from lang_process import TitleProcessor
 
 
-def processWriteToDB(records, db):
-    """Do all the steps with records.
+class Work:
+    def __init__(self, db):
+        self.db = db
+        self.title_processor = None
 
-    1. Check for already seen links, delete seen.
-    2. Filter languages. Delete records with not desired langs.
-    3. Language process the titles. Do each step of TitleProcessor
-    separately and write each of the results to DB.
+    def filterRecords(self, records):
+        # this only saves the work of processing
+        # titles again that are in DB already
+        i = 0
+        end = len(records)
+        while i < end:
+            if self.db.isLinkInDB(records[i][0]):
+                del records[i]
+                end -= 1
+            else:
+                i += 1
 
-    Args:
-        records (list): List of tuples, obtained from getTNVideoInfo
-        db (DB): DB object
-    """
+        self.title_processor = TitleProcessor([rec[1] for rec in records])
+        self.title_processor.filterDesiredLang()
 
-    # this only saves the work of processing
-    # titles again that are in DB already
-    i = 0
-    end = len(records)
-    while i < end:
-        if db.isLinkInDB(records[i][0]):
-            del records[i]
-            end -= 1
-        else:
-            i += 1
+        i = 0
+        end = len(records)
+        while i < end:
+            if records[i][1] not in self.title_processor.selection:
+                del records[i]
+                end -= 1
+            else:
+                i += 1
 
-    title_processor = TitleProcessor([rec[1] for rec in records])
-    title_processor.filterDesiredLang()
+    def processWriteToDB(self, records):
+        """Do all the steps with records.
 
-    i = 0
-    end = len(records)
-    while i < end:
-        if records[i][1] not in title_processor.selection:
-            del records[i]
-            end -= 1
-        else:
-            i += 1
+        1. Check for already seen links, delete seen.
+        2. Filter languages. Delete records with not desired langs.
+        3. Language process the titles. Do each step of TitleProcessor
+        separately and write each of the results to DB.
 
-    db.insertYTRawRecords(records)
+        Args:
+            records (list): List of tuples, obtained from getTNVideoInfo
+            db (DB): DB object
+        """
 
-    title_processor.tokenizeAlnum()
-    toks_pure = [" ".join(toklist) for toklist in title_processor.processed]
-    title_processor.removeStopwords()
-    no_stop = [" ".join(toklist) for toklist in title_processor.processed]
-    title_processor.lemmatize()
-    lemmas = [" ".join(toklist) for toklist in title_processor.processed]
+        self.db.insertYTRawRecords(records)
 
-    db.insertProcessedRecords(
-        list(zip([rec[0] for rec in records], toks_pure, no_stop, lemmas))
-    )
+        # title_processor = TitleProcessor([rec[1] for rec in records])
+        self.title_processor.tokenizeAlnum()
+        toks_pure = [" ".join(toklist) for toklist in self.title_processor.processed]
+        self.title_processor.removeStopwords()
+        no_stop = [" ".join(toklist) for toklist in self.title_processor.processed]
+        self.title_processor.lemmatize()
+        lemmas = [" ".join(toklist) for toklist in self.title_processor.processed]
+
+        self.db.insertProcessedRecords(
+            list(zip([rec[0] for rec in records], toks_pure, no_stop, lemmas))
+        )
