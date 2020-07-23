43ef39e36d6833f769e7ba083c04d3966e44c3c9
ullu <ullu@ullu.de>
2020-07-23 21:18:26 +0200

    Try some models.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import sys
 
@@ -7,7 +9,7 @@ import nltk
 from nltk.classify import TextCat
 from nltk.corpus import stopwords
 
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, FastText
 from nltk.corpus import brown, treebank, movie_reviews
 
 from config import Config
@@ -22,13 +24,20 @@ if __name__ == "__main__":
     webdriver_path = config.webdriver_path
     prof_path = config.prof_path
 
-    browser = Browser(webdriver_path, prof_path)
-    scraper = Scraper(browser)
-
     db = DB()
 
-    gui = GUI(browser, scraper, db)
-    gui.run()
+    # browser = Browser(webdriver_path, prof_path)
+    # scraper = Scraper(browser)
+
+    # gui = GUI(browser, scraper, db)
+    # gui.run()
+
+    lemmas = [tup[0].split() for tup in db.loadProcessed("lemmatized")]
+    print(lemmas)
+    ft = FastText(lemmas, min_count=1)
+    print(ft.wv.most_similar(["philosophy", "science", "truth"]))
+    # w2v = Word2Vec(lemmas, min_count=1)
+    # print(w2v.wv.most_similar(["left", "new"]))
 
     # db2 = DB("ytdata_new.sqlite3")
     # tninfo = [tup[1:] for tup in db.loadRaw()]
@@ -36,45 +45,3 @@ if __name__ == "__main__":
     # work = Work(db2)
     # work.filterRecords(tninfo)
     # work.processWriteToDB(tninfo)
-
-    # w2v = Word2Vec(lemmas, min_count=1)
-    # print(w2v.wv.most_similar("best"))
-
-    # el = browser.driver.find_element_by_link_text(tninfo[0][1])
-    # el.click()
-
-    # titles = [tup[1] for tup in tninfo]
-    # print(tninfo)
-    # print(titles)
-
-    # title_processor = TitleProcessor(titles, LANGS, NLTK_DATA)
-    # title_processor.processTitles()
-
-    # print(title_processor.processed)
-    # db = DB()
-    # # for tup in tninfo:
-    # select = tninfo[:]
-    # for i in range(len(tninfo)):
-    #     if tninfo[i][1] not in title_processor.selection:
-    #         del select[i]
-    # db.insertYTRawRecords(select)
-    # select.append(
-    #     (
-    #         "/watch?v=DUMMYBLABLA",
-    #         "Mdou Moctar - Tarhatazed (Live on KEXP)",
-    #         "KEXP",
-    #         1200000,
-    #         8760,
-    #         0,
-    #     )
-    # )
-    # db.insertYTRawRecords(select)
-
-    # rows = db.loadRaw()
-    # print(rows)
-
-    # proc = ("/watch?v=IQwqmutHqWA",) + tuple(
-    #     " ".join(t) for t in [["asda", "asdsd"], ["rrrrr", "rteret"], ["sdfd", "yyyy"]]
-    # )
-
-    # db.insertProcessedRecords([proc])
