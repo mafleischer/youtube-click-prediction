3821b8ffe872b91ddc4b35f39b331d330cef664a
ullu <ullu@ullu.de>
2020-07-28 21:14:46 +0200

    Integrate similarity code into GUI (raw).
    For a try.

--- gui.py
+++ gui.py
@@ -2,7 +2,11 @@ from tkinter import Tk, Frame, Button, Listbox
 from tkinter import RIGHT, LEFT, TOP, BOTTOM, END
 from tkinter import SINGLE
 
+# tmp
+import numpy as np
+
 from db_lang_workers import Work
+from models import FT
 
 
 def fn():
@@ -15,6 +19,7 @@ class GUI:
         self.browser = browser
         self.scraper = scraper
         self.db = db
+        self.ft = FT(self.db)
         self.records = []  # used subsequently for listbox
 
         self.root = Tk()
@@ -75,9 +80,62 @@ class GUI:
         work.processWriteToDB(tninfo)
         self.records = tninfo
         self.listbox.delete(0, END)
+        links = []
         for rec in tninfo:
+            links.append(rec[0])
             title = rec[1]
+
+            print(title)
+
             self.listbox.insert(END, title)
+
+        # train model and compute click likelihood for scraped videos
+
+        tok_strings_scraped = self.db.loadProcessed(
+            self.db.yt_proc_cols["lemma"], links=tuple(links)
+        )
+
+        toks = [s[0].split() for s in tok_strings_scraped]
+        self.ft.model.build_vocab(toks, update=True)
+        self.ft.model.train(
+            toks,
+            total_examples=self.ft.model.corpus_count,
+            total_words=self.ft.model.corpus_total_words,
+            epochs=self.ft.model.epochs,
+        )
+
+        self.ft.save()
+
+        scraped_avg_vecs = []
+        for s in tok_strings_scraped:
+            tok_vecs = []
+            for tok in s:
+                vec = self.ft.model.wv.word_vec(tok)
+                tok_vecs.append(vec)
+            scraped_avg_vecs.append(np.mean(np.array(tok_vecs), axis=0))
+
+        tok_strings_clicked = [
+            tup[0] for tup in self.db.loadClickedProc(self.db.yt_proc_cols["lemma"])
+        ]
+        clicked_avg_vecs = []
+        for s in tok_strings_clicked:
+            tok_vecs = []
+            for tok in s:
+                vec = self.ft.model.wv.word_vec(tok)
+                tok_vecs.append(vec)
+            clicked_avg_vecs.append(np.mean(np.array(tok_vecs), axis=0))
+
+        avg_clicked = np.mean(np.array(clicked_avg_vecs), axis=0)
+
+        # avg_scraped = np.mean(np.array(scraped_avg_vecs), axis=0)
+        # print(self.ft.model.wv.cosine_similarities(avg_clicked, [avg_scraped]))
+        similarities = list(
+            zip(
+                self.ft.model.wv.cosine_similarities(avg_clicked, scraped_avg_vecs),
+                tok_strings_scraped,
+            )
+        )
+        print(similarities)
 
     def clickClickSelection(self):
         sel = self.listbox.curselection()
