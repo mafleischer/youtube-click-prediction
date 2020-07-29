2a60df50cd317a618d8ef8963cab4a00019820e6
ullu <ullu@ullu.de>
2020-07-29 18:08:19 +0200

    Add inc. train and similarity.
    Train fasttext model on scraped titles.
    
    Print click scores.

--- gui.py
+++ gui.py
@@ -21,6 +21,7 @@ class GUI:
         self.db = db
         self.ft = FT(self.db)
         self.records = []  # used subsequently for listbox
+        self.click_prob = []  # holds probabilites / similarities of scraped titles
 
         self.root = Tk()
         self.root.title("YouTube - Predict next click")
@@ -84,62 +85,34 @@ class GUI:
         for rec in tninfo:
             links.append(rec[0])
             title = rec[1]
-
-            print(title)
-
             self.listbox.insert(END, title)
 
-        # train model and compute click likelihood for scraped videos
-
-        tok_strings_scraped = self.db.loadProcessed(
-            self.db.yt_proc_cols["lemma"], links=tuple(links)
-        )
-
-        toks = [s[0].split() for s in tok_strings_scraped]
-        self.ft.model.build_vocab(toks, update=True)
-        self.ft.model.train(
-            toks,
-            total_examples=self.ft.model.corpus_count,
-            total_words=self.ft.model.corpus_total_words,
-            epochs=self.ft.model.epochs,
-        )
-
-        self.ft.save()
-
-        scraped_avg_vecs = []
-        for s in tok_strings_scraped:
-            tok_vecs = []
-            for tok in s:
-                vec = self.ft.model.wv.word_vec(tok)
-                tok_vecs.append(vec)
-            scraped_avg_vecs.append(np.mean(np.array(tok_vecs), axis=0))
-
-        tok_strings_clicked = [
-            tup[0] for tup in self.db.loadClickedProc(self.db.yt_proc_cols["lemma"])
-        ]
-        clicked_avg_vecs = []
-        for s in tok_strings_clicked:
-            tok_vecs = []
-            for tok in s:
-                vec = self.ft.model.wv.word_vec(tok)
-                tok_vecs.append(vec)
-            clicked_avg_vecs.append(np.mean(np.array(tok_vecs), axis=0))
-
-        avg_clicked = np.mean(np.array(clicked_avg_vecs), axis=0)
-
-        # avg_scraped = np.mean(np.array(scraped_avg_vecs), axis=0)
-        # print(self.ft.model.wv.cosine_similarities(avg_clicked, [avg_scraped]))
-        similarities = list(
-            zip(
-                self.ft.model.wv.cosine_similarities(avg_clicked, scraped_avg_vecs),
-                tok_strings_scraped,
-            )
-        )
-        print(similarities)
+        self.ft.incTrain(tuple(links))
+        self.click_prob = self.ft.similScraped2Clicked(tuple(links))
 
     def clickClickSelection(self):
+        """Simulate user click on video and print scores.
+
+        Pass selected link to Browser object, i.e. selenium.
+        Print scores and whether selection was probable next click.
+        """
         sel = self.listbox.curselection()
         if sel:
             link = self.records[sel[0]][0]
             self.browser.clickLink(link)
             self.db.updateClicked(link)
+
+            argsort = np.argsort(-1 * np.array(self.click_prob))  # descending sort
+
+            prob_sort = np.array(self.click_prob)[argsort]
+            rec_sort = np.array(self.records)[argsort]
+
+            if link in rec_sort[:3, 0]:
+                print("\n+++ HIT! +++ Video was in 3 most probable.\n")
+            else:
+                print("\n--- MISS! --- Video was not in 3 most probable.\n")
+
+            print(
+                "##################\n" + "Scores and titles:\n" + "##################\n"
+            )
+            print(list(zip(prob_sort, rec_sort[:, 1])))
