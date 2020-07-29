513f271f9abfba426fae77cff66c54dc968418d0
ullu <ullu@ullu.de>
2020-07-29 18:11:06 +0200

    Uncomment.

--- main.py
+++ main.py
@@ -29,16 +29,15 @@ if __name__ == "__main__":
 
     db = DB()
 
-    # browser = Browser(webdriver_path, prof_path)
-    # scraper = Scraper(browser)
+    browser = Browser(webdriver_path, prof_path)
+    scraper = Scraper(browser)
 
-    # gui = GUI(browser, scraper, db)
-    # gui.run()
+    gui = GUI(browser, scraper, db)
+    gui.run()
 
-    lemmas = [tup[0].split() for tup in db.loadProcessed("lemmatized")]
-    # print(lemmas)
+    # lemmas = [tup[0].split() for tup in db.loadProcessed("lemmatized")]
 
-    ft = FT(db)
+    # ft = FT(db)
     # ft.save()
 
     # vecs1 = np.array(
@@ -51,48 +50,36 @@ if __name__ == "__main__":
     # )
     # vecs2 = np.array(
     #     [
-    #         ft.model.wv.word_vec("jordan"),
-    #         ft.model.wv.word_vec("peterson"),
-    #         ft.model.wv.word_vec("truth"),
     #         ft.model.wv.word_vec("joe"),
-    #     ]
-    # )
-    # vecs2 = np.array(
-    #     [
-    #         ft.model.wv.word_vec("philosopy"),
+    #         ft.model.wv.word_vec("rogan"),
     #         ft.model.wv.word_vec("science"),
-    #         ft.model.wv.word_vec("brain"),
     #         ft.model.wv.word_vec("math"),
     #     ]
     # )
-    vecs2 = np.array(
-        [
-            ft.model.wv.word_vec("joe"),
-            ft.model.wv.word_vec("rogan"),
-            ft.model.wv.word_vec("science"),
-            ft.model.wv.word_vec("math"),
-        ]
-    )
+    # vecs3 = np.array(
+    #     [
+    #         ft.model.wv.word_vec("read"),
+    #         ft.model.wv.word_vec("men"),
+    #         ft.model.wv.word_vec("science"),
+    #         ft.model.wv.word_vec("peterson"),
+    #     ]
+    # )
 
     # avg1 = np.mean(vecs1, axis=0)
-    avg2 = np.mean(vecs2, axis=0)
-    # print(ft.model.wv.cosine_similarities(avg1, [avg2]))
-    # print(ft.model.wv.wmdistance(["science", "brain"], ["science", "brainwaves"]))
+    # avg2 = np.mean(vecs2, axis=0)
+    # avg3 = np.mean(vecs3, axis=0)
 
-    strs = [tup[0] for tup in db.loadClickedProc(db.yt_proc_cols["lemma"])]
-    clicked_vecs = []
-    for s in strs:
-        title_vecs = []
-        for word in s:
-            vec = ft.model.wv.word_vec(word)
-            title_vecs.append(vec)
-        clicked_vecs.append(np.mean(np.array(title_vecs), axis=0))
+    # strs = [tup[0] for tup in db.loadClickedProc(db.yt_proc_cols["lemma"])]
+    # clicked_vecs = []
+    # for s in strs:
+    #     title_vecs = []
+    #     for word in s:
+    #         vec = ft.model.wv.word_vec(word)
+    #         title_vecs.append(vec)
+    #     clicked_vecs.append(np.mean(np.array(title_vecs), axis=0))
 
-    avg_clicked = np.mean(np.array(clicked_vecs), axis=0)
-    print(ft.model.wv.cosine_similarities(avg_clicked, [avg2]))
-
-    # w2v = Word2Vec(lemmas, min_count=1)
-    # print(w2v.wv.wmdistance(["left", "new"]))
+    # avg_clicked = np.mean(np.array(clicked_vecs), axis=0)
+    # print(ft.model.wv.cosine_similarities(avg_clicked, [avg1, avg2, avg3]))
 
     # db2 = DB("ytdata_new.sqlite3")
     # tninfo = [tup[1:] for tup in db.loadRaw()]
